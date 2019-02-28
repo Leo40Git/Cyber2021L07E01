edcb649931e68a792cf7eff2580a09a4712113f3
Kfir Awad <kfir40mailminer@gmail.com>
2019-02-28 09:03:01 +0200

    finish

--- app/src/main/java/com/leo/cyber2021l07e01/MainActivity.java
+++ app/src/main/java/com/leo/cyber2021l07e01/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import static java.lang.Double.NaN;
+import static java.lang.Double.isNaN;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String FORMAT_STR = "%.10f";
@@ -25,11 +28,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        num1 = Double.NaN;
-        num2 = Double.NaN;
-        lastResult = Double.NaN;
-
         etDisplay = findViewById(R.id.etDisplay);
+
+        reset();
+        lastResult = NaN;
     }
 
     private double input;
@@ -44,28 +46,89 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    public void onClick_opAdd(View view) {
+    private void calculate(Operator newOp) {
+        if (isNaN(num1)) {
+            if (newOp == null)
+                return;
+            if (!getInput())
+                return;
+            num1 = input;
+            op = newOp;
+            etDisplay.setText("");
+            return;
+        } else if (isNaN(num2)) {
+            if (!getInput())
+                return;
+            num2 = input;
+            if (op == null && newOp != null)
+                op = newOp;
+            etDisplay.setText("");
+        }
+        switch (op) {
+            case ADD:
+                lastResult = num1 + num2;
+                break;
+            case SUBTRACT:
+                lastResult = num1 - num2;
+                break;
+            case MULTIPLY:
+                lastResult = num1 * num2;
+                break;
+            case DIVIDE:
+                if (num2 == 0) {
+                    num2 = NaN;
+                    Toast.makeText(this, "Can't divide by zero!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                lastResult = num1 / num2;
+                break;
+            default:
+                Toast.makeText(this, "Operation not chosen??", Toast.LENGTH_SHORT).show();
+                return;
+        }
+        op = newOp;
+        num2 = NaN;
+        String resStr = String.format(FORMAT_STR, lastResult);
+        if (op == null) {
+            num1 = NaN;
+            etDisplay.setText(resStr);
+            etDisplay.setHint("");
+        } else {
+            num1 = lastResult;
+            etDisplay.setText("");
+            etDisplay.setHint(resStr);
+        }
+    }
 
+    private void reset() {
+        op = null;
+        num1 = NaN;
+        num2 = NaN;
+        etDisplay.setText("");
+    }
+
+    public void onClick_opAdd(View view) {
+        calculate(Operator.ADD);
     }
 
     public void onClick_opSub(View view) {
-
+        calculate(Operator.SUBTRACT);
     }
 
     public void onClick_opMul(View view) {
-
+        calculate(Operator.MULTIPLY);
     }
 
     public void onClick_opDiv(View view) {
-
+        calculate(Operator.DIVIDE);
     }
 
     public void onClick_clear(View view) {
-
+        reset();
     }
 
     public void onClick_equ(View view) {
-
+        calculate(null);
     }
 
     public void onClick_credits(View view) {
